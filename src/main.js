ebcfbb9207a1302b1ead6b65bdc888d86a208acb
Ярослав <yar.91r@gmail.com>
2026-08-01 20:18:26 +0300

    Первая версия сайта

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+<div class="page">
+    <div class="heart">❤️</div>
+
+    <h1>Привет, Поль!</h1>
+
+    <p>
+        Этот сайт существует только потому,
+        что есть ты.
+    </p>
+
+    <button>
+        Нажми сюда ❤️
+    </button>
+</div>
+`
