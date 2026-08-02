d95dc870e52abd0b5bd0c3809305f1449ce1e236
Ярослав <yar.91r@gmail.com>
2026-08-02 15:44:29 +0300

    Добавил анимацию

--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    for (let i = 0; i < 20; i++) {
+        createHeart();
+    }
+});
+
+function createHeart() {
+    const heart = document.createElement("div");
+
+    heart.innerHTML = "💖";
+    heart.style.position = "fixed";
+    heart.style.left = Math.random() * window.innerWidth + "px";
+    heart.style.top = window.innerHeight + "px";
+    heart.style.fontSize = "30px";
+    heart.style.transition = "3s linear";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.style.top = "-100px";
+        heart.style.opacity = "0";
+    }, 50);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 3000);
+}
+
 <div class="page">
     <div class="heart">❤️</div>
 
